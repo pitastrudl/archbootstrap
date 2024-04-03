86a1f4140c86613c567c4fa6fcfd1ac14bc17cc1
Arun Bahl <pitastrudl@gmail.com>
2024-04-03 23:26:49 +0200

    init pacman key on start of bootstrap

--- archlive/airootfs/root/bootstrap.sh
+++ archlive/airootfs/root/bootstrap.sh
@@ -1,3 +1,4 @@
+pacman-key --init
 pacman -Syu --noconfirm archinstall git ansible
 git clone https://github.com/pitastrudl/archbootstrap
 archinstall --skip-version-check  --config archbootstrap/user_configuration.json
