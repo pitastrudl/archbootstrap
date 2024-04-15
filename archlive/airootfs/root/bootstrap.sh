9b9adec7893c7fba38184d6a273f96fc383bd057
Arun Bahl <pitastrudl@gmail.com>
2024-04-15 20:13:23 +0200

    do not update as we are in live iso

--- archlive/airootfs/root/bootstrap.sh
+++ archlive/airootfs/root/bootstrap.sh
@@ -4,7 +4,7 @@ gpg --refresh-keys
 pacman-key --populate
 pacman-key --refresh-keys
 systemctl start archlinux-keyring-wkd-sync
-pacman -S --noconfirm archlinux-keyring
-pacman -Syu --noconfirm git
+pacman -Sy --noconfirm archlinux-keyring
+pacman -Sy --noconfirm git
 git clone https://github.com/pitastrudl/archbootstrap
 cd archbootstrap && bash start.sh
