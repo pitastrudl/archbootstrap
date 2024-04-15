84581023d71f9a242ad18e304c4ea45a67ab757c
Arun Bahl <pitastrudl@gmail.com>
2024-04-15 20:18:06 +0200

    possibly unneded steps

--- archlive/airootfs/root/bootstrap.sh
+++ archlive/airootfs/root/bootstrap.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 pacman-key --init
-gpg --refresh-keys
 pacman-key --populate
-pacman-key --refresh-keys
 systemctl start archlinux-keyring-wkd-sync
 pacman -Sy --noconfirm archlinux-keyring
 pacman -Sy --noconfirm git
